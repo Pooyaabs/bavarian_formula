3e33e14a6a7fcd3b496b1672469b3df2c57f9d18
Pooyaabs <122750393+Pooyaabs@users.noreply.github.com>
2023-01-15 21:21:41 +0100

    Update bavarian_calculator.py
    updated the small error

--- bavarian_calculator.py
+++ bavarian_calculator.py
@@ -4,7 +4,7 @@ b = int(input("Enter the minimum score in the grading system: "))
 c = int(input("Enter the current grade of the student: "))
 
 #Calculate the bavarian formula
-result = (a-c/a-b)*3+1
+result = ((a-c)/(a-b))*3+1
 
 #Print the result
 print("The result of the Bavarian formula is: ", result)
